b2a9bf33a226c49f6433c426956c48cadc45b10e
Cristine de Noronha <cristinegdn@gmail.com>
2022-10-15 22:56:00 -0400

    js3

--- deNoronha_Cristine_favoriteThings/js/main.js
+++ deNoronha_Cristine_favoriteThings/js/main.js
@@ -4,14 +4,13 @@
         createApp({
         created() {
         console.log('Vue Instance is Created');
-        //Fetch the remote data here and pass it to the data object
-    
+   
     fetch('./data.json')
         .then(res => res.json())
         .then(data => {
         console.log(data);
         
-    //Push the data into the vue instance
+
     //The "this" keyword inside of the Vue instance ALWAYS refers to the instance itself
         this.remoteData = data;
      })
